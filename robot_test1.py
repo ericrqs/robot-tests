75b2d84accc3ac63be13f4bd35792823a3bf6a28
ericrqs <eric.r@quali.com>
2017-03-16 12:39:17 -0400

    initial commit

---
+++ robot_test1.py
@@ -0,0 +1,14 @@
+import random
+import time
+
+
+def test1_func(cloudshell_reservation_id, cloudshell_server_address, cloudshell_port, cloudshell_username, cloudshell_password, cloudshell_domain, cloudshell_reservation_json):
+    print('hello from master')
+    print('CloudShell reservation id: %s' % cloudshell_reservation_id)
+    print('CloudShell server address: %s' % cloudshell_server_address)
+    print('CloudShell port: %d' % int(cloudshell_port))
+    print('CloudShell admin username: %s' % cloudshell_username)
+    print('CloudShell admin password: %s' % '(hidden)')
+    print('CloudShell domain: %s' % cloudshell_domain)
+    print('CloudShell reservation details: %s' % cloudshell_reservation_json)
+    time.sleep(random.randint(5, 30))
---
+++ robot_test_fail.py
@@ -0,0 +1,15 @@
+import random
+import time
+
+
+def test_fail_func(cloudshell_reservation_id, cloudshell_server_address, cloudshell_port, cloudshell_username, cloudshell_password, cloudshell_domain, cloudshell_reservation_json):
+    print('hello from ericr_dev')
+    print('CloudShell reservation id: %s' % cloudshell_reservation_id)
+    print('CloudShell server address: %s' % cloudshell_server_address)
+    print('CloudShell port: %d' % int(cloudshell_port))
+    print('CloudShell admin username: %s' % cloudshell_username)
+    print('CloudShell admin password: %s' % '(hidden)')
+    print('CloudShell domain: %s' % cloudshell_domain)
+    print('CloudShell reservation details: %s' % cloudshell_reservation_json)
+    time.sleep(random.randint(5, 30))
+    raise Exception('Intentionally throwing an exception from Robot Python test')
