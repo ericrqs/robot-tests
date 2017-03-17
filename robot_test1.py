8fada210e0c6f346e068e9245c67ad3f2e978ebc
ericrqs <eric.r@quali.com>
2017-03-17 10:38:05 -0400

    add command line input to test1

--- robot_test1.py
+++ robot_test1.py
@@ -2,7 +2,8 @@ import random
 import time
 
 
-def test1_func(cloudshell_reservation_id, cloudshell_server_address, cloudshell_port, cloudshell_username, cloudshell_password, cloudshell_domain, cloudshell_reservation_json):
+def test1_func(cloudshell_reservation_id, cloudshell_server_address, cloudshell_port, cloudshell_username,
+               cloudshell_password, cloudshell_domain, cloudshell_reservation_json, extrainput):
     print('hello from latest on master branch')
     print('')
     print('CloudShell reservation id: %s' % cloudshell_reservation_id)
@@ -12,4 +13,5 @@ def test1_func(cloudshell_reservation_id, cloudshell_server_address, cloudshell_
     print('CloudShell admin password: %s' % '(hidden)')
     print('CloudShell domain: %s' % cloudshell_domain)
     print('CloudShell reservation details: %s' % cloudshell_reservation_json)
+    print('Command line input 1: %s' % extrainput)
     time.sleep(random.randint(5, 10))
